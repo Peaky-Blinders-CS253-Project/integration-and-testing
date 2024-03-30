498b359a33492d9bb7e33ebbca50599598d3737a
amany222 <135151457+amany222@users.noreply.github.com>
2024-03-30 23:35:54 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,23 +1,25 @@
+#!/bin/bash
+
+# Install packages from requirements.txt
+echo "Installing Python packages from requirements.txt..."
+python3 -m pip install -r requirements.txt
+
 # Ensure pip is installed
 if ! command -v pip &> /dev/null
 then
-    echo "pip could not be found"
-    echo "Installing pip..."
-    sudo apt update
-    sudo apt install python3-pip
+    echo "pip could not be found, installing..."
+    sudo apt-get update
+    sudo apt-get install -y python3-pip
 fi
-
-# Install Python packages
-echo "Installing Python packages..."
-pip install -r requirements.txt
 
 # Ensure Django is installed
 if ! python3 -c "import django" &> /dev/null
 then
-    echo "Django could not be found"
-    echo "Installing Django..."
+    echo "Django could not be found, installing..."
     pip install django
 fi
+
+
 
 # Collect static files
 echo "Collecting static files..."
